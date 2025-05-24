961a04e682fa964d6dda81ce8c6b48d886f21be7
Habacuque <habacuke14@gmail.com>
2025-05-24 18:00:22 -0300

    fix bug crash in flow of navegation bar

--- app/src/main/java/com/example/growup/ui/course/IndexCoursesActivity.java
+++ app/src/main/java/com/example/growup/ui/course/IndexCoursesActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.growup.R;
@@ -13,22 +14,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class IndexCoursesActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index_courses);
 
-        if (savedInstanceState == null){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, new CourseListFragment())
-                    .commit();
-        }
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
 
     }
 
